429c5705bf99c27300b685d3bcfab1e75a6f2e27
Adehope-Tech <y5d87@students.keele.ac.uk>
2025-11-14 10:48:02 +0000

    introduce a limit of  5-guess limit

--- Game.py
+++ Game.py
@@ -1,20 +1,29 @@
 import random
 
 print("--- Welcome to 'Guess the Number' ---")
-# Set the number to guess
 secret_number = random.randint(1, 20)
-print("I'm thinking of a number between 1 and 20.")
+print("I'm thinking of a number between 1 and 20. You have 5 guesses.")
 
 guess = 0
-# Loop until the user guesses correctly
-while guess != secret_number:
-    try:
-        guess = int(input("Take a guess: "))
-        if guess < secret_number:
-            print("Too low!")
-        elif guess > secret_number:
-            print("Too high!")
-    except ValueError:
-        print("Please enter a valid number.")
+guess_count = 0
+guess_limit = 5
+out_of_guesses = False
 
-print(f"You got it! The number was {secret_number}.")
+while guess != secret_number and not out_of_guesses:
+    if guess_count < guess_limit:
+        try:
+            guess = int(input("Take a guess: "))
+            guess_count += 1
+            if guess < secret_number:
+                print("Too low!")
+            elif guess > secret_number:
+                print("Too high!")
+        except ValueError:
+            print("Please enter a valid number.")
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print(f"You're out of guesses! The number was {secret_number}.")
+else:
+    print(f"You got it in {guess_count} guesses! The number was {secret_number}.")
